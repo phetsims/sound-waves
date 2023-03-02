de417db8b64963222e7a6eaafab3ecad67d17066
phet-dev <phetadmin@gmail.com>
2023-03-02 05:04:12 -0700

    update copyright dates from daily grunt work

--- js/common/view/PropertyControlSlider.ts
+++ js/common/view/PropertyControlSlider.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 /**
  * Slider that controls a given property, can display the current value and a title.
  *
